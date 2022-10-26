ca8f6ea2c2f5c4c862c3de4fd83021ccd2b3f73c
Daoud LAMALMI <102736413+MadokaIII@users.noreply.github.com>
2022-10-26 14:19:34 +0200

    Superclass Humain créé

---
+++ src/personnages/Humain.java
@@ -0,0 +1,45 @@
+package personnages;
+
+public class Humain {
+
+	String nom;
+	String boisson;
+	int argent;
+
+	public Humain(String nom, String boisson, int argent) {
+		this.nom = nom;
+		this.boisson = boisson;
+		this.argent = argent;
+	}
+
+	public void parler(String texte) {
+		System.out.println("(" + nom + ") : " + "« " + texte + " »");
+	}
+
+	public void direBonjour() {
+		parler("Bonjour ! Je m’appelle " + nom + " et j’aime boire du " + boisson);
+	}
+
+	public void boire() {
+		parler("Mmmm, un bon verre de " + boisson + " ! GLOUPS !");
+	}
+
+	public void gagnerArgent(int gain) {
+		argent += gain;
+	}
+
+	public void perdreArgent(int perte) {
+		argent -= perte;
+	}
+
+	public void acheter(String bien, int prix) {
+		if (prix <= argent) {
+			parler("J'ai " + argent + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix + " sous.");
+			perdreArgent(prix);
+		} else {
+			parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir " + bien + " à " + prix
+					+ " sous.");
+		}
+	}
+
+}
